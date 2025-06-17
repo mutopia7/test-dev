be7c5e19aac3df85994445dc2904657de894af1a
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-06-17 20:52:33 +0330

    Add analyzeArray function for getting  average, min, max and length of array

---
+++ analyzeArray.js
@@ -0,0 +1,60 @@
+let sample = [1,8,3,4,2,6]
+
+sample.reduce
+
+function mergeSort(array) {
+    if (array.length <= 1) {
+        return array; // آرایه یک‌عضوی یا خالی را مستقیم برمی‌گردانیم
+    }
+
+    const mid = Math.floor(array.length / 2);
+    const left = array.slice(0, mid);
+    const right = array.slice(mid);
+
+    const sortedLeft = mergeSort(left);
+    const sortedRight = mergeSort(right);
+
+    return merge(sortedLeft, sortedRight); // ترکیب دو نیمه مرتب شده
+}
+
+
+
+
+function merge(leftArray, rightArray) {
+    const newArray = []
+    let i = 0;
+    let j = 0;
+
+    while (i < leftArray.length && j < rightArray.length) {
+        if (leftArray[i] < rightArray[j]) {
+            newArray.push(leftArray[i]);
+            i++;
+        } else if (rightArray[j] < leftArray[i]) {
+            newArray.push(rightArray[j]);
+            j++;
+        } else {
+            newArray.push(leftArray[i]);
+            newArray.push(rightArray[j]);
+            i++;
+            j++;
+        }
+
+    }
+
+     // اگر یکی از آرایه‌ها هنوز عناصر باقی‌مانده دارد
+    return [...newArray, ...leftArray.slice(i), ...rightArray.slice(j)];
+}
+
+function analyzeArray(array){
+    const sortedArray = mergeSort(array);
+    let result = {
+        average: sortedArray.reduce((pre, next) => pre + next) / sortedArray.length,
+        min: sortedArray[0],
+        max: sortedArray[sortedArray.length - 1],
+        length: sortedArray.length
+
+    }
+    return result
+}
+
+console.log(analyzeArray(sample))
